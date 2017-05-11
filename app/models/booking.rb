03851c4116d1d8330aa17e6e7fc212ebd5e60122
loic cessot <loic.cessot@gadz.org>
2017-05-11 18:40:45 +0200

    suppress uniqueness

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  validates :date_check_in, uniqueness: true, presence: true
-  validates :date_check_out, uniqueness: true, presence: true
+  validates :date_check_in, presence: true
+  validates :date_check_out, presence: true
   belongs_to :user
   belongs_to :boat
 end
