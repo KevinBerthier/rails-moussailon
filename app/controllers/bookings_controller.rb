e9efcce668b23ada82709e6a44466f01a8d0bd7f
Kevin Berthier <kevinberthier@orange.fr>
2017-05-12 16:26:14 +0200

    edit profile add photo, name , lastname

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,12 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @booking.date_check_in
     @boat = Boat.find(params[:boat_id])
   end
 
+  def create
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   mount Attachinary::Engine => "/attachinary"
+  get  "/edit-profile", to: "users#edit"
+  patch  "/edit-profile", to: "users#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :boats do
     resources :bookings, only:[:new, :create]
