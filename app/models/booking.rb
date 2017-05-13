3b4a355fa16aaf526d20324cd3bc7c7d0e0428f4
Kevin Berthier <kevinberthier@orange.fr>
2017-05-13 11:55:41 +0200

    new booking process ok + dashboard

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   validates :date_check_in, presence: true
   validates :date_check_out, presence: true
+  validates :total_price, numericality: true, presence: true
   belongs_to :user
   belongs_to :boat
 end
