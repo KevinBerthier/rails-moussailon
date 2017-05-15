8d5c64f35fa8904ef7152174492044d7d75a39af
Amelie Marcombes <amelie.marcombes@gmail.com>
2017-05-15 15:59:56 +0200

    show button dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -42,8 +42,6 @@ ActiveRecord::Schema.define(version: 20170515124405) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
