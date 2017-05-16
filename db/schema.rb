61011e1adc721076d42d8af85b91614f835d4cc0
Amelie Marcombes <amelie.marcombes@gmail.com>
2017-05-16 22:20:10 +0200

    super modal pour contacter un locataire

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170516122726) do
+ActiveRecord::Schema.define(version: 20170516155842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
