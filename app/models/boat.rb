b2ce022fd44d636766615c6538ade2ceab9aa640
loic cessot <loic.cessot@gadz.org>
2017-05-17 12:03:38 +0200

    debug API?

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,12 +2,14 @@ class Boat < ApplicationRecord
   GENDER = ["Voilier", "Bateau à moteur", "Catamaran", "Semi-rigide", "Yacht"]
   belongs_to :user
   has_many :bookings
+  has_attachments :photos, maximum: 3
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   validates :name, presence: true
   validates :address, presence: true
   validates :city, presence: true
   validates :price, numericality: true
   validates :capacity, numericality: true
-  has_attachments :photos, maximum: 3
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
 end
